25ddbe8f43864868baa00b29322d4e4a8501d300
Marshall <marshall_tower@yahoo.ca>
2020-07-10 06:38:22 -0600

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (evt) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
         response.json().then((data) => {
             if (data.error) messageOne.textContent = data.error
             else {
